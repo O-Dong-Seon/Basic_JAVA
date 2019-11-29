de85ae7d63c1519fdb26f4b875529c691f261fec
O-Dong-Seon <58206101+O-Dong-Seon@users.noreply.github.com>
2019-11-29 11:16:37 +0900

    파파터치 햄버거 POS 프로그램 완성
    2차원배열,while,for,scanner
    대-소문자변환,형변환

--- BasicJava/src/problem/fafatouch/fafamain.java
+++ BasicJava/src/problem/fafatouch/fafamain.java
@@ -52,78 +52,79 @@ public class fafamain {
 
 		// {"코카콜라, 스프라이트, 마운틴듀, 오렌지쥬스, 초코쉐이크"};
 		// 음료메뉴를 저장하는 5칸짜리 배열 생성
-
-		System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
-		System.out.println("▒ 파파터치 주문 프로그램");
-		System.out.println("▒ ◆ 메인메뉴 ◆");
-		System.out.println("▒ 1.파파버거");
-		System.out.println("▒ 2.새우버거");
-		System.out.println("▒ 3.와퍼버거");
-		System.out.println("▒ 4.싸이버거");
-		System.out.println("▒ 5.빅맥버거");
-		System.out.print("▒ 번호 >>");
-		// 사용자가 주문한 메인메뉴 번호
-		int mainNum = sc.nextInt();
-
-		System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
-		System.out.println("▒ 파파터치 주문 프로그램");
-		System.out.println("▒ ◆ 사이드메뉴 ◆");
-		System.out.println("▒ 1.감자튀김");
-		System.out.println("▒ 2.양념감자");
-		System.out.println("▒ 3.치즈스틱");
-		System.out.println("▒ 4.맥너겟");
-		System.out.println("▒ 5.스위트콘");
-		System.out.print("▒ 번호 >>");
-		// 사용자가 주문한 사이드메뉴 번호
-		int sideNum = sc.nextInt();
-
-		System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
-		System.out.println("▒ 파파터치 주문 프로그램");
-		System.out.println("▒ ◆ 음료 ◆");
-		System.out.println("▒ 1.코카콜라");
-		System.out.println("▒ 2.스프라이트");
-		System.out.println("▒ 3.마운틴듀");
-		System.out.println("▒ 4.오렌지쥬스");
-		System.out.println("▒ 5.초코쉐이크");
-		System.out.print("▒ 번호 >>");
-		// 사용자가 주문한 음료번호
-		int drinkNum = sc.nextInt();
-
-		// 총주문금액 계산!
-		// 메인메뉴 가격 mainMenu[mainNum=1][1]
-		// 사이드메뉴 가격 sideMenu[sideNum=1][1]
-		// 음료메뉴 가격 drinkMenu[drinkNum=1][1]
-		int mPrice = Integer.parseInt(mainMenu[mainNum - 1][1]);
-		int sPrice = Integer.parseInt(sideMenu[sideNum - 1][1]);
-		int dPrice = Integer.parseInt(drinkMenu[drinkNum - 1][1]);
-		int totprice = mPrice + sPrice + dPrice;
-
-		// 출력!!
-		System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
-		System.out.println("▒ 고객님께서 주문하신");
-		System.out.println("▒ 1)" + mainMenu[mainNum - 1][0] + ":" + mainMenu[mainNum - 1][1]); // 메인메뉴
-		System.out.println("▒ 2)" + sideMenu[sideNum - 1][0] + ":" + sideMenu[sideNum - 1][1]); // 사이드메뉴
-		System.out.println("▒ 3)" + drinkMenu[drinkNum - 1][0] + ":" + drinkMenu[drinkNum - 1][1]); // 음료메뉴
-		System.out.println("▒ 총 금액은" + totprice + "원 입니다");
-		System.out.print("▒입니다. 주문하시겠습니까?(y/n)>>");
-		// 엔터키는 2가지 기능을 가지고 있음
-		// 1.입력기능
-		// 2.한줄개행(\n)
-		// JAVA에서 엔터를 입력하면 1번과 2번이 실행됨
-		// sc.nextInt()는 정수값만 받기때문에 한줄개행
-		// 코드를 무시하지만 sc.nextLine()은 문자열을
-		// 입력받아 한줄개행코드를 입력으로 받음...
-		// sc.nextLine()을 사용해서 한줄개행입력을
-		// 대신받아주는 부분이 필요함!!
-		sc.nextLine();
+		while(true) {
+			System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+			System.out.println("▒ 파파터치 주문 프로그램");
+			System.out.println("▒ ◆ 메인메뉴 ◆");
+			System.out.println("▒ 1.파파버거");
+			System.out.println("▒ 2.새우버거");
+			System.out.println("▒ 3.와퍼버거");
+			System.out.println("▒ 4.싸이버거");
+			System.out.println("▒ 5.빅맥버거");
+			System.out.print("▒ 번호 >>");
+			// 사용자가 주문한 메인메뉴 번호
+			int mainNum = sc.nextInt();
+	
+			System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+			System.out.println("▒ 파파터치 주문 프로그램");
+			System.out.println("▒ ◆ 사이드메뉴 ◆");
+			System.out.println("▒ 1.감자튀김");
+			System.out.println("▒ 2.양념감자");
+			System.out.println("▒ 3.치즈스틱");
+			System.out.println("▒ 4.맥너겟");
+			System.out.println("▒ 5.스위트콘");
+			System.out.print("▒ 번호 >>");
+			// 사용자가 주문한 사이드메뉴 번호
+			int sideNum = sc.nextInt();
+	
+			System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+			System.out.println("▒ 파파터치 주문 프로그램");
+			System.out.println("▒ ◆ 음료 ◆");
+			System.out.println("▒ 1.코카콜라");
+			System.out.println("▒ 2.스프라이트");
+			System.out.println("▒ 3.마운틴듀");
+			System.out.println("▒ 4.오렌지쥬스");
+			System.out.println("▒ 5.초코쉐이크");
+			System.out.print("▒ 번호 >>");
+			// 사용자가 주문한 음료번호
+			int drinkNum = sc.nextInt();
+	
+			// 총주문금액 계산!
+			// 메인메뉴 가격 mainMenu[mainNum=1][1]
+			// 사이드메뉴 가격 sideMenu[sideNum=1][1]
+			// 음료메뉴 가격 drinkMenu[drinkNum=1][1]
+			int mPrice = Integer.parseInt(mainMenu[mainNum - 1][1]);
+			int sPrice = Integer.parseInt(sideMenu[sideNum - 1][1]);
+			int dPrice = Integer.parseInt(drinkMenu[drinkNum - 1][1]);
+			int totprice = mPrice + sPrice + dPrice;
+	
+			// 출력!!
+			System.out.println("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒");
+			System.out.println("▒ 고객님께서 주문하신");
+			System.out.println("▒ 1)" + mainMenu[mainNum - 1][0] + ":" + mainMenu[mainNum - 1][1]); // 메인메뉴
+			System.out.println("▒ 2)" + sideMenu[sideNum - 1][0] + ":" + sideMenu[sideNum - 1][1]); // 사이드메뉴
+			System.out.println("▒ 3)" + drinkMenu[drinkNum - 1][0] + ":" + drinkMenu[drinkNum - 1][1]); // 음료메뉴
+			System.out.println("▒ 총 금액은" + totprice + "원 입니다");
+			System.out.print("▒입니다. 주문하시겠습니까?(y/n)>>");
+			// 엔터키는 2가지 기능을 가지고 있음
+			// 1.입력기능
+			// 2.한줄개행(\n)
+			// JAVA에서 엔터를 입력하면 1번과 2번이 실행됨
+			// sc.nextInt()는 정수값만 받기때문에 한줄개행
+			// 코드를 무시하지만 sc.nextLine()은 문자열을
+			// 입력받아 한줄개행코드를 입력으로 받음...
+			// sc.nextLine()을 사용해서 한줄개행입력을
+			// 대신받아주는 부분이 필요함!!
+			sc.nextLine();
 		String order = sc.nextLine();
 		System.out.println(order);
-
-		if (order.equals("y")) {
+		// order.toUpperCase(); // 대문자변환
+		String smallAns = order.toLowerCase(); // 소문자변환
+		if (smallAns.equals("y")) {
 
 			while (true) {
 				// 결제금액 입력
-				System.out.println("▒ 결제금액 >>");
+				System.out.print("▒ 결제금액 >>");
 				int money = sc.nextInt();
 				if (totprice > money) {
 					// 지불금액이 총구매액보다 작은 경우!!
@@ -131,16 +132,25 @@ public class fafamain {
 					continue;
 				} else {
 					// 지불금액(10000) > 총구매액 (5000)
-					if(totprice < money) {
+					if (totprice < money) {
 						System.out.println((money - totprice) + "거스름돈 받아가세요");
 					}
+					// 지불금액 == 결제금액
+					// 지불금액 > 결제금액(거스름돈 주고)
+					System.out.println("▒결제완료! 고객님 맛있게드세요! ㅋㅅㅋ");
 					break;
 				}
 			}
-		} else {
-			// 시스템에 처음으로 돌아가세요
+		} else if(order.equals("n") || order.equals("N")){
+			//시스템에 처음으로 돌아가세요
+			continue;
+		}else {
+			System.out.println("y 나 n을 입력해주세요");
+			continue;
 		}
-
+			
+			//sc.close(); // 사용한 자원 반납
+		}
 	}
 
 }
